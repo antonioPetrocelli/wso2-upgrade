768c02b5a8398f311ae8a6758e112152ccd9742d
Antonio Petrocelli <antonio.petrocelli@gmail.com>
2024-12-10 07:13:19 +0100

    alert sintassi

--- is/600to610/db/postgres/IDENTITY_DB-600to610.sql
+++ is/600to610/db/postgres/IDENTITY_DB-600to610.sql
@@ -1,3 +1,5 @@
+-- Attenzione: verificare la sintassi per ADD, ALTER e DROP CONSTRAINT
+
 ALTER TABLE IDN_OAUTH2_DEVICE_FLOW ADD COLUMN SUBJECT_IDENTIFIER VARCHAR(255);
 INSERT INTO IDN_CONFIG_TYPE (ID, NAME, DESCRIPTION) VALUES ('7f24050f-3e3d-4a00-b10f-fd5450d6523e', 'input-validation-configurations', 'A resource type to store input validation related configurations');
 ALTER TABLE SP_SHARED_APP ADD COLUMN SHARE_WITH_ALL_CHILDREN BOOLEAN DEFAULT FALSE;
